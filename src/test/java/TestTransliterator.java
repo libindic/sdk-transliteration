7c9b1d1530e7eb9fd6eb971a2e6a69fdfe3df127
Sujith V <sujithvm24@gmail.com>
2014-07-09 01:32:55 +0530

    Add tests for transliteration

---
+++ src/test/java/TestTransliterator.java
@@ -0,0 +1,35 @@
+import android.test.AndroidTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.silpa.transliteration.Transliterator;
+
+/**
+ * Created by sujith on 7/7/14.
+ */
+public class TestTransliterator extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @SmallTest
+    @MediumTest
+    @LargeTest
+    public void testTransliterate() {
+
+        Transliterator transliterator = new Transliterator(getContext());
+        assertEquals("namaskaara idu kannaDa paThya TraansliTareshhan parikshhegaagi", transliterator.transliterate("ನಮಸ್ಕಾರ ಇದು ಕನ್ನಡ ಪಠ್ಯ ಟ್ರಾನ್ಸ್ಲಿಟರೇಷನ್ ಪರೀಕ್ಷೆಗಾಗಿ", "en_US"));
+        assertEquals("namaskaar silpaa kee duniyaa mem aapakaa svaagath hai", transliterator.transliterate("नमस्कार सिल्पा की दुनिया में आपका स्वागत है", "en_US"));
+        assertEquals("ದಿಸ್ ಇಸ್ ಅ ಇಂಗ್ಗ್ಲಿಷ್ ಟೆಕ್ಸ್ಟ್ ಫೋರ್ transliteration", transliterator.transliterate("This is a english text for transliteration", "kn_IN"));
+        assertEquals("நமஸ்கார இது கந்நட பட்ய ட்ராந்ஸ்லிடரேஷந் பரீக்ஷெகாகி", transliterator.transliterate("ನಮಸ್ಕಾರ ಇದು ಕನ್ನಡ ಪಠ್ಯ ಟ್ರಾನ್ಸ್ಲಿಟರೇಷನ್ ಪರೀಕ್ಷೆಗಾಗಿ", "ta_IN"));
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
